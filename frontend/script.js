969012d31ffa6fb52a4452a5aad92b4be747b412
Bruno Modeste <bmodeste@oui.sncf>
2025-06-23 15:22:34 +0200

    exo suite

---
+++ frontend/script.js
@@ -0,0 +1,16 @@
+// Écoute l'événement DOMContentLoaded pour s'assurer que le DOM est entièrement chargé avant d'exécuter le script
+document.addEventListener("DOMContentLoaded", async () => {
+
+    // TODO : Récupération de l'élément pokemon-list
+
+    // Fin TODO Récupération de l'élément pokemon-list
+
+    // TODO : Récupération des données des Pokémon depuis l'API avec fetch
+
+    // Fin TODO Récupération des données des Pokémon depuis l'API avec fetch
+
+    // TODO : Affichage de la liste de Pokémon dans l'élément pokemon-list
+    // - Afficher la liste de pokemon avec l'image et le nom de chaque Pokémon
+
+    // fin TODO Affichage de la liste de Pokémon dans l'élément pokemon-list
+});
